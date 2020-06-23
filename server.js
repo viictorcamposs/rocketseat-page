e2a01e55e85307d4d4b334ec5c1ac10334ef34d8
Victor Campos <campos.viictor@gmail.com>
2020-06-23 16:58:00 -0300

    instalando nodemon

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require ('express')
 
 const server = express ()
 
+server.get ('/', function (req, res) {
+  return res.send ()
+})
+
 server.listen (5000, function () {
-  console.log ('Server is Running')
+  console.log ('Server is Running') 
 })
