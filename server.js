897df8138c7fa5f7c822d087db8f8eee63af3b5d
Victor Campos <campos.viictor@gmail.com>
2020-06-25 15:07:25 -0300

    trabalhando com dados dinâmicos na home page

--- server.js
+++ server.js
@@ -26,7 +26,41 @@ nunjucks.configure ('views', {
 })
 
 server.get ('/', function (req, res) {
-  return res.render ('home')
+  const cards = [
+    {
+      name: "Javascript",
+      image: "https://rocketseat.com.br/static/images/update/curso-javascript.svg",
+      title: "Domine a linguagem de programação",
+      subtitle: "Acompanhe o avanço da linguagem"
+    },
+    {
+      name: "NodeJS",
+      image: "https://rocketseat.com.br/static/images/update/curso-nodejs.svg",
+      title: "Construa a base da sua aplicação no back-end",
+      subtitle: "Para controlar a lógica e os dados das aplicações web e mobile"
+    },
+    {
+      name: "ReactJS",
+      image: "https://rocketseat.com.br/static/images/update/curso-reactjs.svg",
+      title: "Desenvolva a versão web da sua aplicação",
+      subtitle: "Para construir interfaces responsivas e dinâmicas no front-end"
+    },
+    {
+      name: "React Native",
+      image: "https://rocketseat.com.br/static/images/update/curso-react-native.svg",
+      title: "Crie as versões mobile da sua aplicação",
+      subtitle: "Para desenvolver apps para iOS e Android com interface nativa"
+    }
+  ]
+  const mainDescription = [
+    {
+      title: "Rocketseat",
+      logoImage: "https://media-exp1.licdn.com/dms/image/C510BAQFHYedTI-oyWA/company-logo_200_200/0?e=1600905600&v=beta&t=GlUqRmwprv-OUQwKtpUvzxtjsL8BP5EMlEfeAt1ZM1Y",
+      description: "Mais do que uma plataforma de educação em tecnologia, uma comunidade incrível de devs em busca do próximo nível",
+      linkUrl: "https://rocketseat.com.br/"
+    }
+  ]
+  return res.render ('home', { cards, wrapper: mainDescription })
 })
 server.get ('/bootcamp-page.njk', function (req, res) {
   return res.render ('bootcamp-page', { items: data }) 
