be28ec09c0ee8ce3f9a50a2c45636b8d73f23190
Victor Campos <campos.viictor@gmail.com>
2020-06-26 16:13:57 -0300

    removendo conteúdo modal da minha bootcamp-page

--- public/scripts/bootcamp-page.js
+++ public/scripts/bootcamp-page.js
@@ -1,33 +1,10 @@
-const modalOverlay = document.querySelector ('.modal-overlay')
 const cards = document.querySelectorAll ('.card')
-
-
-/* MANIPULANDO CLOSE-MODAL */
-document.querySelector ('.close-modal').addEventListener ('click', () => {
-  modalOverlay.classList.remove ('active')
-  if ( modalOverlay.classList.contains ('maximize') ) {
-    modalOverlay.classList.remove ('maximize')
-  }
-})
-
-/* MANIPULANDO MAXIMIZE */
-document.querySelector ('.fullscreen').addEventListener ('click', () => {
-  if ( modalOverlay.classList.contains ('maximize') ) {
-    modalOverlay.classList.remove ('maximize')
-  } else {
-    modalOverlay.classList.add ('maximize')
-  }
-})
-
 
 /* MANIPULANDO CARDS */
 for (let card of cards) {
   card.addEventListener ('click', () => {
-    modalOverlay.classList.add ('active')
     /* MANIPULANDO MODAL IFRAME */
     const idPage = card.getAttribute ('id')
-    modalOverlay.querySelector ('iframe').src = `https://www.rocketseat.com.br/${idPage}`
-
   })
 }
 
